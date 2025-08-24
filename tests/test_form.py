9e01d44486838568945c590da8691471a6257fbc
Igor Davydov <maink@list.ru>
2025-08-24 13:59:06 +0300

    add_allure_in_test

--- tests/test_form.py
+++ tests/test_form.py
@@ -1,4 +1,5 @@
 import pytest
+import allure
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
@@ -17,6 +18,26 @@ def driver():
     yield driver
     driver.quit()
 
+@allure.step("Ввод логина: {username}")
+def enter_username(element, username):
+    element.send_keys(username)
+
+@allure.step("Ввод пароля")
+def enter_password(element, password):
+    element.send_keys(password)
+
+@allure.step("Нажатие кнопки входа")
+def click_submit(button):
+    button.click()
+
+@allure.title("Тест успешной авторизации")
+@allure.description("""
+Проверка корректной авторизации с валидными учетными данными:
+- Ввод корректного имени пользователя
+- Ввод корректного пароля
+- Проверка успешного сообщения
+""")
+@allure.feature("Авторизация")
 def test_successful_login(driver):
     # Открываем страницу авторизации
     driver.get("https://the-internet.herokuapp.com/login")
@@ -26,16 +47,23 @@ def test_successful_login(driver):
     password = driver.find_element(By.ID, "password")
     submit_button = driver.find_element(By.CSS_SELECTOR, "button[type='submit']")
 
-    username.send_keys("tomsmith")
-    password.send_keys("SuperSecretPassword!")
-    submit_button.click()
+    enter_username(username, "tomsmith")
+    enter_password(password, "SuperSecretPassword!")
+    click_submit(submit_button)
 
     # Проверяем, авторизованы
     success_message = driver.find_element(By.ID, "flash")
     assert "You logged into a secure area!" in success_message.text
     assert driver.current_url == "https://the-internet.herokuapp.com/secure"
 
-
+@allure.title("Тест неуспешной авторизации")
+@allure.description("""
+Проверка обработки ошибки при вводе невалидных учетных данных:
+- Ввод некорректного имени пользователя
+- Ввод некорректного пароля
+- Проверка сообщения об ошибке
+""")
+@allure.feature("Авторизация")
 def test_unsuccessful_login(driver):
     # Открываем страницу авторизации
     driver.get("https://the-internet.herokuapp.com/login")
@@ -45,9 +73,9 @@ def test_unsuccessful_login(driver):
     password = driver.find_element(By.ID, "password")
     submit_button = driver.find_element(By.CSS_SELECTOR, "button[type='submit']")
 
-    username.send_keys("tomsmithhhhh")
-    password.send_keys("SuperSecretPassworddddddd")
-    submit_button.click()
+    enter_username(username, "tomsmithhhhh")
+    enter_password(password, "SuperSecretPassworddddddd")
+    click_submit(submit_button)
 
     # Проверяем, что ввели ошибочные данные
     error_message = driver.find_element(By.ID, "flash")
